b4fcdb455e9e365c3314a494f9705f05b010e6b3
Tanguy <tanguy.leg@gmail.com>
2019-05-23 16:57:40 +0200

    WIP TIcketController

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Ticket;
+/* use App\Http\Requests\CreateTicketRequest;
+use App\Http\Requests\GetTicketRequest; */
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Enumerators\ComparatorEnum;
 
 
 class TicketsController extends Controller
@@ -14,7 +17,7 @@ class TicketsController extends Controller
      * Create ticket
      * @param createTicket $request
      */
-    public function createTicket(Request $request){
+    public function createTicket(CreateTicketRequest $request){
         $queries = $request -> all();
         $datas = [];
 
@@ -34,22 +37,53 @@ class TicketsController extends Controller
     /**
      * Get ticket
      */
-    public function getTicketById(Request $request, $id) {   
-        return response('test');
+    public function getTicketById(GetTicketRequest $request, $id) { 
+
+        $resData = DB::table('tickets')
+            ->select()
+            ->where('id', '=', $id)
+            ->get();
+
+        if (sizeof($resData) === 0) {
+            $json_error = array('message' => "No data available for this Id", 'status' => "404");
+            $json_error = json_encode($json_error);
+            return response($json_error, 404);
+        }
+
+        return response($resData, 200);
     }
 
      /**
      * Get ticket by params
+     * @param Request $request
      */
     public function getTicketsByParams(Request $request) {   
-        $bodyContent -> $request -> getContent();
-        return response('test');
+        $queries = $request -> all();
+        $sql = DB::table('tickets');
+        $wheres = [];
+
+        foreach($queries as $key => $query)
+            foreach($query as $where)
+                array_push($wheres, [
+                    $key,
+                    ComparatorEnum::$operators[$where["operator"]],
+                    $where["comparator"]
+                ]);
+        $sql -> where($wheres);
+        return response($sql -> get(), 200);
     }
 
     /**
      * Update ticket by body
      */
-    public function updateTicket(Request $request) {   
+    public function updateTicket(Request $request, $id) {   
+        $queries = $request -> all();
+        $datas = [];
+
+        foreach($queries as $key => $query)
+            $datas[$key] = $query;
+        
+        $sql = DB::table('tickets')->where('id', '=', $id)->update($datas);
         return response($request, 200);
     }
 
@@ -57,7 +91,16 @@ class TicketsController extends Controller
      * Delete ticket
      */
     public function deleteTicket(Request $request, $id) {   
-        return response($id, 200);
+        $queries = $request -> all();
+
+        try {
+            DB::table('tickets').findOrFail($id)
+        } catch ($th) {
+            return response($th, 404)
+        }
+
+        $sql = DB::table('tickets')->where('id', '=', $id)->delete();
+        return response(null, 204);
     }
 
     /**
